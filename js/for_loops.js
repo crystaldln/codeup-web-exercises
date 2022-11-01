6230084c34496f228fd51e1d74d4061034cd6ad4
Crystal <crystal.m.deleon00@gmail.com>
2022-11-01 18:34:33 -0500

    done

--- js/for_loops.js
+++ js/for_loops.js
@@ -3,29 +3,41 @@
 // Exercise 1 & 2
 
 
-function showMultiplicationTable(number) {
-    for(let i = 1; i <=10; i++){
-        console.log(number * i);
-    }
+ function showMultiplicationTable(number) {
+     for(let i = 1; i <=10; i++){
+         console.log(number * i);
+     }
 
 
-}
+ }
 
-showMultiplicationTable(7);
+ showMultiplicationTable(7);
 
 // Exercise 3
 
+ function getRandomNumber(min,max) {
+  return Math.random() * (max - min) + min;
+ }
+
+ var result = Math.floor(Math.random());
+
+ console.log(Math.random());
+
 
 // Exercise 4
+const loopNumber = 1;
 
+for (let i = 0; i < loopNumber; i++) {
+    console.log(loopNumber);
+}
 
 
 
 
 // Exercise 5
 
- for (var i = 100; i >= 5; i = i - 5) {
-     console.log(i);
- }
+  for (var i = 100; i >= 5; i = i - 5) {
+      console.log(i);
+  }
 
 
