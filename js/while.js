20b8733f61681b20d0600ba189dd877a22aef9f0
Crystal <crystal.m.deleon00@gmail.com>
2022-11-01 19:20:42 -0500

    donr

--- js/while.js
+++ js/while.js
@@ -1,3 +1,5 @@
+// Exercise 1 & 2
+
 let i = 1;
 
 while (i < 65536) {
@@ -6,6 +8,20 @@ while (i < 65536) {
 
 }
 
+// Exercise 3
 
 let allCones = Math.floor(Math.random() * 50) + 50;
+
+do {
+    let conesToBuy = Math.floor(Math.random() * 5) + 1;
+    if ((allCones - conesToBuy) < 0) {
+        console.log("Cannot sell you" + conesToBuy + "cones I only have" + allCones + "...");
+    } else {
+        console.log("# of cones before sale: " + allCones);
+        allCones -= conesToBuy;
+        console.log(conesToBuy + "cones sold.");
+    }
+} while(allCones > 0);
+        console.log("We sold all the cones!!");
+
 
