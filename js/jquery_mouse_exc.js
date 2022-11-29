23b08d08c3fb176db922e457292684445ad9cca7
Crystal <crystal.m.deleon00@gmail.com>
2022-11-28 20:35:56 -0600

    mouse done

---
+++ js/jquery_mouse_exc.js
@@ -0,0 +1,17 @@
+"use strict";
+
+$(function () {
+    $("h1").click(function () {
+        $(this).css("background", "orange");
+    });
+
+    $("p").dbclick(function() {
+        $(this).css("font-size", "18px");
+    });
+
+    $("li").hover(function () {
+        $(this).css("color", "red");
+    }, function () {
+        $(this).css("color", "black");
+    });
+});
