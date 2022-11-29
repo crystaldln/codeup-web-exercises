52c3807f19454f3178d5ea3d53b132264734bb5c
Crystal <crystal.m.deleon00@gmail.com>
2022-11-28 20:14:08 -0600

    changes done

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -6,8 +6,10 @@
        //
        // $(".codeup").css("border", "1px solid red");
 
-       $("li").css("font-size", "20px").css("background", "yellow");
-       $("h1").css("background", "yellow");
-       $("p").css("background", "yellow");
-       alert($("h1").html());
+       // $("li").css("font-size", "20px").css("background", "yellow");
+       // $("h1").css("background", "yellow");
+       // $("p").css("background", "yellow");
+       // alert($("h1").html());
+       $("li").css("font-size", "20px");
+       $("h1, p, li").css("background-color", "green");
 });
