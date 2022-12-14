078a503764d730d65c5f8eae69608c66dced9e08
Crystal <crystal.m.deleon00@gmail.com>
2022-12-14 09:41:20 -0600

    done es6

---
+++ js/jquery_faq.js
@@ -0,0 +1,24 @@
+
+    $("#btn-1").click(function(e) {
+    e.preventDefault();
+    $("dd").toggleClass("invisible");
+});
+
+    $("dt").click(function() {
+    $(this).toggleClass("highlighter");
+});
+
+    $("#btn-2").click(function() {
+    $("ul").each(function () {
+        $(this).children().last().toggleClass("yellow-bg");
+    });
+});
+
+    $("h3").click(function () {
+    $(this).next().children().css("font-weight", "bold");
+});
+
+    $("li").click(function() {
+    $(this).parent().children().first().css("color", "blue");
+});
+
